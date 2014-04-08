dd9ad753b3a9b3bcbd7d73af776e6f60ed85659c
thuongvu <thuongvuho@gmail.com>
2014-04-08 14:12:44 -0700

    add - made it more pop, added randomize button

--- script.js
+++ script.js
@@ -1,44 +1,37 @@
-var FlashCard = function(question, answer) {
+var FlashCard = function(question, answer, topic) {
 	this.question = question;
 	this.answer = answer;
+	this.topic = topic;
 };
 
-FlashCard.prototype.showQuestion = function() {
-	document.getElementById("card-front").innerText = this.question;
-};
-FlashCard.prototype.showAnswer = function() {
-	document.getElementById("card-back").innerText = this.answer;
-};
-
-var HtmlCard = function(question, answer) {
-	FlashCard.call(this, question, answer);
-	this.topic = "html";
-};
-HtmlCard.prototype = new FlashCard();
-HtmlCard.prototype.showTopic = function() {
-	document.getElementById("topic").innerText = this.topic;
-};
-
-var CssCard = function(question, answer) {
-	FlashCard.call(this, question, answer);
-	this.topic = "css";
-};
-CssCard.prototype = new FlashCard();
-CssCard.prototype.showTopic = function() {
-	HtmlCard.prototype.showTopic.call(this);
+FlashCard.prototype = {
+	constructor: FlashCard,
+	showQuestion: function() {
+		document.getElementById("card-front").innerText = this.question;
+	},
+	showAnswer: function() {
+		document.getElementById("card-back").innerText = this.answer;
+	},
+	showTopic: function() {
+		document.getElementById("topic").innerText = this.topic;
+	},
+	cardsArray: [],
+	makeCardsPushIntoArr: function() {
+		for (var i = 0; i < cards.length; i++) {
+				this.cardsArray.push(new FlashCard(cards[i].question, cards[i].answer, cards[i].topic));
+		};
+	}
 }
 
-var JsCard = function(question, answer) {
-	FlashCard.call(this, question, answer);
-	this.topic = "js";
-};
-JsCard.prototype = new FlashCard();
-JsCard.prototype.showTopic = function() {
-	HtmlCard.prototype.showTopic.call(this);
-}
-
-
-
+// FlashCard.prototype.showQuestion = function() {
+// 	document.getElementById("card-front").innerText = this.question;
+// };
+// FlashCard.prototype.showAnswer = function() {
+// 	document.getElementById("card-back").innerText = this.answer;
+// };
+// FlashCard.prototype.showTopic = function() {
+// 	document.getElementById("topic").innerText = this.topic;
+// };
 
 var cards = [
 	{
@@ -53,15 +46,61 @@ var cards = [
 	}
 ];
 
-var htmlArr = [];
-for (var i = 0; i < cards.length; i++) {
-	if (cards[i].topic === "html") {
-		htmlArr.push(new HtmlCard(cards[i].question, cards[i].answer));
+// var cardsArray = [];
+// for (var i = 0; i < cards.length; i++) {
+// 		cardsArray.push(new FlashCard(cards[i].question, cards[i].answer, cards[i].topic));
+// };
+var newObj = new FlashCard();
+
+newObj.makeCardsPushIntoArr();
+console.log(newObj.cardsArray);
+
+// console.log(cardsArray);
+cardsArray[0].showQuestion();
+cardsArray[0].showAnswer();
+cardsArray[0].showTopic();
+
+var EventUtility = {
+	addHandler: function(element, type, handler) {
+		if (element.addEventListener) {
+			element.addEventListener(type, handler, false);
+		} else if (element.attachEvent) {
+			element.attachEvent(type, handler);
+		} else {
+			element["on" + type] = handler;
+		};
+	},
+	removeHandler: function(element, type, handler) {
+		if (element.removeEventListener) {
+			element.removeEventListener(type, handler, false);
+		} else if (element.detachEvent) {
+			element.detachEvent(type, handler);
+		} else {
+			element["on" + type] = null;
+		};
+	},
+	getEvent: function(event) {
+		return event ? event : window.event;
+	},
+	getTarget: function(event) {
+		return event.target || event.srcElement;
 	}
-	
+}
+
+var clickHandler = function() {
+	var target = null;
+	event = EventUtility.getEvent(event);
+	target = EventUtility.getTarget(event);
+	switch (target.id) {
+		case "randomize":
+			console.log("randomize");
+			break;
+	}
 };
 
-console.log(htmlArr);
-htmlArr[0].showQuestion();
-htmlArr[0].showAnswer();
-// htmlArr[0].showTopic();
+EventUtility.addHandler(document.getElementById("randomize"), "click", clickHandler);
+
+
+
+
+
