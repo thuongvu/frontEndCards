da2998d97c6c685e5a32d824e0d7bb6cb8615c37
thuongvu <thuongvuho@gmail.com>
2014-04-08 02:39:49 -0700

    add - combination inheritance with htmlcard, cascara, jscard, but this is too much

--- script.js
+++ script.js
@@ -10,12 +10,44 @@ FlashCard.prototype.showAnswer = function() {
 	document.getElementById("card-back").innerText = this.answer;
 };
 
+var HtmlCard = function(question, answer) {
+	FlashCard.call(this, question, answer);
+	this.topic = "html";
+};
+HtmlCard.prototype = new FlashCard();
+HtmlCard.prototype.showTopic = function() {
+	document.getElementById("topic").innerText = this.topic;
+};
+
+var CssCard = function(question, answer) {
+	FlashCard.call(this, question, answer);
+	this.topic = "css";
+};
+CssCard.prototype = new FlashCard();
+CssCard.prototype.showTopic = function() {
+	HtmlCard.prototype.showTopic.call(this);
+}
+
+var JsCard = function(question, answer) {
+	FlashCard.call(this, question, answer);
+	this.topic = "js";
+};
+JsCard.prototype = new FlashCard();
+JsCard.prototype.showTopic = function() {
+	HtmlCard.prototype.showTopic.call(this);
+}
+
+
+
+
 var cards = [
 	{
+		"topic": "html",
 		"question": "semantic html",
-		"answer": "agreed meaning of html elements, attributes, values"
+		"answer": "agreed meaning of html elements, attributes, values something else"
 	},
 	{
+		"topic": "html",
 		"question": "optimize",
 		"answer": "file concat, minification, cdn, caching, sprites, minimize http calls"
 	}
@@ -23,8 +55,13 @@ var cards = [
 
 var htmlArr = [];
 for (var i = 0; i < cards.length; i++) {
-	htmlArr.push(new FlashCard(cards[i].question, cards[i].answer));
+	if (cards[i].topic === "html") {
+		htmlArr.push(new HtmlCard(cards[i].question, cards[i].answer));
+	}
+	
 };
 
 console.log(htmlArr);
-htmlArr[1].showQuestion();
+htmlArr[0].showQuestion();
+htmlArr[0].showAnswer();
+// htmlArr[0].showTopic();
